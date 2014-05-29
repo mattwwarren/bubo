09e5e59b74866fb84507902fbe00e6005493eb31
Matt Warren <mwarren@wanderu.com>
2014-05-28 22:19:25 -0400

    setting up bubo to use bitbucket or jira interchangably

--- bubo_bot.js
+++ bubo_bot.js
@@ -6,26 +6,34 @@ catch (e) {
   console.log(' -=- > Loading configuration from environment variables');
 }
 var https = require('https');
-var jira = require('jira-api');
 var wobot = require('wobot');
 
-
 // Pull this in now to allow either file-based config (checked second) or environment variables (checked first)
+var debug = process.env.NODE_DEBUG || config.debug || false;
 var runtimeOptions = {
   hipchatUser: process.env.HIPCHAT_USER || config.hipchatUser,
   hipchatPassword: process.env.HIPCHAT_PASSWORD || config.hipchatPassword,
-  jiraBrowseUrl: process.env.JIRA_BROWSE_URL || config.jiraBrowseUrl,
-  jiraHostname: process.env.JIRA_HOSTNAME || config.jiraHostname,
-  jiraUsername: process.env.JIRA_USERNAME || config.jiraUsername,
-  jiraPassword: process.env.JIRA_PASSWORD || config.jiraPassword
+  tracker: process.env.TRACKER || config.tracker
+}
+if (runtimeOptions.tracker == 'bitbucket'){
+    runtimeOptions.bitBucketUrl = process.env.BITBUCKET_URL || config.bitBucketUrl;
+    runtimeOptions.bbUsername = process.env.BB_USERNAME || config.bbUsername;
+    runtimeOptions.bbPassword = process.env.BB_PASSWORD || config.bbPassword;
+    runtimeOptions.bbProjectRe = process.env.BB_PROJECT_RE ? new RegExp(process.env.BB_PROJECT_RE, "g") : config.bbProjectRe;
+} else {
+    runtimeOptions.jiraBrowseUrl = process.env.JIRA_BROWSE_URL || config.jiraBrowseUrl;
+    runtimeOptions.jiraHostname = process.env.JIRA_HOSTNAME || config.jiraHostname;
+    runtimeOptions.jiraUsername = process.env.JIRA_USERNAME || config.jiraUsername;
+    runtimeOptions.jiraPassword = process.env.JIRA_PASSWORD || config.jiraPassword;
+    runtimeOptions.jiraProjectRe = process.env.JIRA_PROJECT_RE ? new RegExp(process.env.JIRA_PROJECT_RE, "g") : config.jiraProjectRe;
 };
 runtimeOptions.hipchatRoomsToJoin = process.env.HIPCHAT_ROOMS_TO_JOIN ? process.env.HIPCHAT_ROOMS_TO_JOIN.split(',') : config.hipchatRoomsToJoin;
-runtimeOptions.jiraProjectRe = process.env.JIRA_PROJECT_RE ? new RegExp(process.env.JIRA_PROJECT_RE, "g") : config.jiraProjectRe;
 
 
 var b = new wobot.Bot({
-  jid: runtimeOptions.hipchatUser + "/bot",
-  password: runtimeOptions.hipchatPassword
+    debug: debug,
+    jid: runtimeOptions.hipchatUser + "/bot",
+    password: runtimeOptions.hipchatPassword
 });
 
 b.connect();
@@ -48,24 +56,29 @@ b.onInvite(function(roomJid, fromJid, reason) {
 b.onMessage(function(channel, from, message) {
   var self = this;
   var alreadyProcessed = [];
-  var matches = message.match(runtimeOptions.jiraProjectRe);
+  var matches = message.match(runtimeOptions.bbProjectRe);
   if (!matches) {
     return;
   }
 
   console.log(' -=- > Looking up JIRA details for ' + message + ' with matches: ' + matches);
-  matches.forEach(function(jiraKey) { 
-    if (alreadyProcessed.indexOf(jiraKey) < 0) {
-      alreadyProcessed.push(jiraKey);
+  matches.forEach(function(issueKey) { 
+    if (alreadyProcessed.indexOf(issueKey) < 0) {
+      alreadyProcessed.push(issueKey);
+      repository = issueKey.replace("-\d*", "");
+      issueNum = issueKey.replace("main-", "");
+
       var options = {
-        auth: runtimeOptions.jiraUsername + ':' + runtimeOptions.jiraPassword,
+        auth: runtimeOptions.bbUsername + ':' + runtimeOptions.bbPassword,
         headers: { 'accept': 'application/json' },
-        hostname: runtimeOptions.jiraHostname,
+        hostname: "bitbucket.org",
         method: 'GET',
-        path: '/rest/api/2/issue/' + jiraKey,
+        path: '/api/1.0/repositories/wanderu/' + repository + '/issues/' + issueNum,
         port: 443
       };
 
+
+      console.log(options);
       var body = '';
       var req = https.request(options, function(res) {
 
@@ -75,12 +88,12 @@ b.onMessage(function(channel, from, message) {
 
         res.on('end', function() {
           try {
-            var jiraData = JSON.parse(body);
-            var clarification = runtimeOptions.jiraBrowseUrl + jiraData.key + ': “' + jiraData.fields.summary + '” marked as ' + jiraData.fields.status.name + ' and assigned to ' + jiraData.fields.assignee.displayName;
+            var bbData = JSON.parse(body);
+            var clarification = runtimeOptions.bitBucketUrl + repository + "/issue/" + bbData.local_id + ': "' + bbData.title + '" marked as ' + bbData.status + ' and assigned to ' + bbData.responsible.display_name;
             self.message(channel, clarification);
           }
           catch (e) {
-            var sorry = '/me could not find ' + jiraKey;
+            var sorry = '/me could not find ' + issueKey;
             console.error(e);
             self.message(channel, sorry);
           }
