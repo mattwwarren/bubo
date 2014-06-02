4eff6feda37c2bd34ac3f37cfd674995065954b9
Matt Warren <mwarren@wanderu.com>
2014-06-01 22:13:36 -0400

    derp

--- plugins/save.js
+++ plugins/save.js
@@ -6,5 +6,5 @@ var save = {
 module.exports = function(module_holder) {
     // the key in this dictionary can be whatever you want
     // just make sure it won't override other modules
-    module_holder['swear'] = swear;
+    module_holder['save'] = save;
 };
