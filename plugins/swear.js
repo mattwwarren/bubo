9bfd4e57fe5d81e6d32617b79c3ee314a20e20b3
Matt Warren <mwarren@wanderu.com>
2014-12-28 21:04:27 -0500

    swear bug fix

--- plugins/swear.js
+++ plugins/swear.js
@@ -2,7 +2,7 @@ var swear = exports;
 
 swear.is_match = function(message){
     var runtimeOptions = require('../config');
-    var swear_re = new RegExp(runtimeOptions.mentionName + ".*(ass(hole)?|bastard|bitch|fuck|shit)+(\W|$)", "gim");
+    var swear_re = new RegExp(runtimeOptions.mentionName + " (ass(hole)?|bastard|bitch|fuck|shit)+", "gim");
     return message.match(swear_re);
 };
 
